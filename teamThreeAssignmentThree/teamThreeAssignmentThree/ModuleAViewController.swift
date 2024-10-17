3b4615427e8f5529770ba0084ae6a08a0419d2bb
PingHe87 <heping.m87@gmail.com>
2024-10-16 22:28:00 -0500

    解决了闪退问题

--- teamThreeAssignmentThree/teamThreeAssignmentThree/ModuleAViewController.swift
+++ teamThreeAssignmentThree/teamThreeAssignmentThree/ModuleAViewController.swift
@@ -6,24 +6,98 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ModuleAViewController: UIViewController {
+    
+    let motionModel = MotionModel()
+    var stepGoal: Int = 1000 // 默认步数目标
+
+    // MARK: - Outlets
+    @IBOutlet weak var stepsTodayLabel: UILabel!
+    @IBOutlet weak var stepsYesterdayLabel: UILabel!
+    @IBOutlet weak var stepsGoalTextField: UITextField!
+    @IBOutlet weak var stepsRemainingLabel: UILabel!
+    @IBOutlet weak var activityLabel: UILabel!
+    @IBOutlet weak var stepsProgressLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+
+    var currentSteps: Int = 0 // 用于保存当前步数
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // 设置 delegate
+        self.motionModel.delegate = self
+
+        // 开始监控步数和活动状态
+        self.motionModel.startActivityMonitoring()
+        self.motionModel.startPedometerMonitoring()
+
+        // 获取昨天的步数
+        self.motionModel.getYesterdaysSteps { steps in
+            DispatchQueue.main.async {
+                self.stepsYesterdayLabel.text = "Yesterday's Steps: \(Int(steps))"
+            }
+        }
+
+        // 加载用户设置的目标步数
+        loadStepGoal()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - 目标步数功能
+    @IBAction func saveStepGoal(_ sender: UIButton) {
+        if let goalText = stepsGoalTextField.text, let goal = Int(goalText) {
+            UserDefaults.standard.set(goal, forKey: "stepGoal")
+            stepGoal = goal
+            updateStepsRemaining(currentSteps: currentSteps) // 使用当前步数更新剩余步数
+        }
     }
-    */
 
+    func loadStepGoal() {
+        stepGoal = UserDefaults.standard.integer(forKey: "stepGoal")
+        stepsGoalTextField.text = "\(stepGoal)"
+    }
+
+    // 更新剩余步数和进度条
+    func updateStepsRemaining(currentSteps: Int) {
+        let remainingSteps = stepGoal - currentSteps
+        stepsRemainingLabel.text = "Steps to Goal: \(remainingSteps)"
+        
+        // 更新进度条
+        let progress = Float(currentSteps) / Float(stepGoal)
+        progressBar.setProgress(progress, animated: true)
+        
+        // 显示步数进度
+        stepsProgressLabel.text = "\(currentSteps)/\(stepGoal)"
+    }
+}
+
+// MARK: - MotionDelegate
+extension ModuleAViewController: MotionDelegate {
+    func activityUpdated(activity: CMMotionActivity) {
+        var activityType = "Unknown"
+        if activity.walking {
+            activityType = "🚶Walking"
+        } else if activity.running {
+            activityType = "🏃Running"
+        } else if activity.cycling {
+            activityType = "🚴Cycling"
+        } else if activity.automotive {
+            activityType = "🚘Driving"
+        } else if activity.stationary {
+            activityType = "Stationary"
+        }
+        activityLabel.text = "Current Activity: \(activityType)"
+    }
+
+    func pedometerUpdated(pedData: CMPedometerData) {
+        DispatchQueue.main.async {
+            self.currentSteps = pedData.numberOfSteps.intValue // 保存当前步数
+            self.stepsTodayLabel.text = "Today's Steps: \(self.currentSteps)"
+            
+            // 更新进度条和剩余步数
+            self.updateStepsRemaining(currentSteps: self.currentSteps)
+        }
+    }
 }
--- teamThreeAssignmentThree/teamThreeAssignmentThree/MotionModel.swift
+++ teamThreeAssignmentThree/teamThreeAssignmentThree/MotionModel.swift
@@ -6,3 +6,81 @@
 //
 
 import Foundation
+import CoreMotion
+
+// setup a protocol for the ViewController to be delegate for
+protocol MotionDelegate {
+    // Define delegate functions
+    func activityUpdated(activity:CMMotionActivity)
+    func pedometerUpdated(pedData:CMPedometerData)
+}
+
+class MotionModel{
+    
+    // MARK: =====Class Variables=====
+    private let activityManager = CMMotionActivityManager()
+    private let pedometer = CMPedometer()
+    var delegate:MotionDelegate? = nil
+    
+    // MARK: =====Motion Methods=====
+    func startActivityMonitoring(){
+        // is activity is available
+        if CMMotionActivityManager.isActivityAvailable(){
+            // update from this queue (should we use the MAIN queue here??.... )
+            self.activityManager.startActivityUpdates(to: OperationQueue.main)
+            {(activity:CMMotionActivity?)->Void in
+                // unwrap the activity and send to delegate
+                // using the real time pedometer might influences how often we get activity updates...
+                // so these updates can come through less often than we may want
+                if let unwrappedActivity = activity,
+                   let delegate = self.delegate {
+                    // Print if we are walking or running
+                    print("%@",unwrappedActivity.description)
+                    
+                    // Call delegate function
+                    delegate.activityUpdated(activity: unwrappedActivity)
+                    
+                }
+            }
+        }
+        
+    }
+    
+    func startPedometerMonitoring(){
+        // check if pedometer is okay to use
+        if CMPedometer.isStepCountingAvailable(){
+            // start updating the pedometer from the current date and time
+            pedometer.startUpdates(from: Date())
+            {(pedData:CMPedometerData?, error:Error?)->Void in
+                
+                // if no errors, update the delegate
+                if let unwrappedPedData = pedData,
+                   let delegate = self.delegate {
+                    
+                    delegate.pedometerUpdated(pedData:unwrappedPedData)
+                }
+
+            }
+        }
+    }
+    
+    // ASS3: Get Yesterday Steps
+    func getYesterdaysSteps(completion: @escaping (Double) -> Void) {
+            let calendar = Calendar.current
+            let startOfYesterday = calendar.date(byAdding: .day, value: -1, to: calendar.startOfDay(for: Date()))!
+            let endOfYesterday = calendar.startOfDay(for: Date())
+
+            if CMPedometer.isStepCountingAvailable() {
+                pedometer.queryPedometerData(from: startOfYesterday, to: endOfYesterday) { (pedData, error) in
+                    if let pedData = pedData {
+                        completion(pedData.numberOfSteps.doubleValue)
+                    } else {
+                        print("Error retrieving yesterday's steps: \(String(describing: error))")
+                        completion(0)
+                    }
+                }
+            }
+        }
+    
+    
+}
