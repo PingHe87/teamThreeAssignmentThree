6d870927fe44284a22c370170d46302c6a46f56c
PingHe87 <heping.m87@gmail.com>
2024-10-16 22:16:15 -0500

    ModuleA

---
+++ teamThreeAssignmentThree/teamThreeAssignmentThree/MotionModel.swift
@@ -0,0 +1,8 @@
+//
+//  MotionModel.swift
+//  teamThreeAssignmentThree
+//
+//  Created by p h on 10/16/24.
+//
+
+import Foundation
---
+++ teamThreeAssignmentThree/teamThreeAssignmentThree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teamThreeAssignmentThree
+//
+//  Created by p h on 10/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
